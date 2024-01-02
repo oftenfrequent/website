be8916ea47e38abace9f79260dfb1fc186dd11d5
Kyle Burke <k23burke@gmail.com>
2024-01-02 18:52:24 +0100

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,13 +11,6 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
-    // errorElement: <ErrorPage />,
-    // loader: rootLoader,
-    // action: rootAction,
-    // children: [
-    //   { index: true, element: <Index /> },
-    //   /* existing routes */
-    // ],
   },
   {
     path: "/resume",
@@ -30,12 +23,6 @@ export default class AppRoutes extends React.Component {
   render() {
     return (
       <RouterProvider router={router} />
-      // <BrowserRouter>
-      //   <Routes>
-      //     <Route path="/" exact component={HomePage}/>
-      //     <Route path="/resume" component={ResumePage}/>
-      //   </Routes>
-      // </BrowserRouter>
     )
   }
 }
